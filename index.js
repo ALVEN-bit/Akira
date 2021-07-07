012b9e85922baa88750dded64f09636b211a2ed3
AL V EN <78151999+ALVEN-bit@users.noreply.github.com>
2021-07-08 02:41:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -306,15 +306,7 @@ Server id: __${guild.id}__
 Server Count: __${guild.memberCount}__**`);
 });
 
-const channelid = "860205351819411456"    //id channele vc
-client.on("ready", () => {
-    const channel = client.channels.cache.get(channelid);
-    if (!channel) return console.error("The channel does not exist!");
-    channel.join().then(connection => {
-        console.log("Successfully connected.");
-    }).catch(e => {
-        console.error(e);
-    });
+
 });
 
 client.on("message", message => {
