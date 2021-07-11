9a36e92f988553abfd4281c6d52374225a448d8b
AL V EN <78151999+ALVEN-bit@users.noreply.github.com>
2021-07-11 14:52:30 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -20,13 +20,16 @@ module.exports = {
     .setTitle(`**Pop Music Help**`)
     .setDescription(`
 
+◈ ━━━━━━━━ ● ━━━━━━━━ ◈
 
 <a:emoji_17:855062869934080030> | **User Commands**
 \`invite\` - \`support\` - \`about\`
 \`ping\` - \`prefix\` - \`uptime\`
 \`tinvites\` - \`sug\` - \`roleuser\`
 \`report\` - \`se\` - \`avatar\`
- 
+
+◈ ━━━━━━━━ ● ━━━━━━━━ ◈
+
 <a:emoji_39:862473578799955969> | **Music Commands**
 \`play\` - \`skip\` - \`skipto\`
 \`stop\` - \`volume\` - \`nowplaying\`
@@ -34,27 +37,36 @@ module.exports = {
 \`remove\` - \`queue\` - \`loop\`
 \`lyrics\` - \`radio\`
 
+◈ ━━━━━━━━ ● ━━━━━━━━ ◈
+
 <a:emoji_19:855064009639329812> | **Moderation Commands**
 \`ban\` - \`unban\` - \`setnick\`
 \`roleadd\` - \`roleremove\` - \`vkick\`
 \`setprefix\` - \`clear\` - \`close\`
 \`open\` - \`senddm\`
- 
+
+◈ ━━━━━━━━ ● ━━━━━━━━ ◈
+
 <a:emoji_33:856616986422935583> | **Gif Commands**
 \`boy\` - \`girl\` - \`couple\`
 \`sad\` - \`cartoon\` - \`anime\`
 \`neon\`
+
+◈ ━━━━━━━━ ● ━━━━━━━━ ◈
 
 <a:emoji_41:863748588915523604> | **Text Commands**
 \`textkurdish\` - \`textenglish\`
 \`textarabic\` - \`textturkish\`
 \`textpersian\`
 
+◈ ━━━━━━━━ ● ━━━━━━━━ ◈
+
 <a:emoji_40:862473590213705768> | **Game Commands**
 \`kiss\` - \`slap\` - \`hug\`
 \`feed\` - \`cuddle\` - \`pat\`
 \`poke\`
 
+◈ ━━━━━━━━ ● ━━━━━━━━ ◈
 
 <a:emoji_24:856306109940629544> | **Links**
 [__**Support**__](https://discord.gg/wTESwYkdwY) | [__**Invite**__](https://discord.com/api/oauth2/authorize?client_id=828308993172045845&permissions=8&scope=bot)
