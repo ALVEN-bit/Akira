fb33aaaae1689a447cee05bc0988a2b4321327a7
AL V EN <78151999+ALVEN-bit@users.noreply.github.com>
2021-08-01 16:32:14 +0300

    Update textenglish.js

--- others/textenglish.js
+++ others/textenglish.js
@@ -13,8 +13,8 @@ let replies = ["Never bend your head. Always hold it high. Look the world straig
 
 
  message.channel.send(replies[result]).then(message => {
- 			message.react('<a:emoji_91:842129244343369808>').then(r => {
- 				message.react('<a:emoji_94:842141553262460968>');
+ 			message.react('<a:alven:871381046007050253>').then(r => {
+ 				message.react('<a:alven:871381176928059412>');
             });
            });
        }
