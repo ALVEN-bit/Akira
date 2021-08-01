83c1dcd9f5e6d2bfac0347f2a651fc276c3c2d44
AL V EN <78151999+ALVEN-bit@users.noreply.github.com>
2021-08-01 16:35:16 +0300

    Update textarabic.js

--- others/textarabic.js
+++ others/textarabic.js
@@ -14,8 +14,8 @@ let replies = ["هيه حظوظ والطيب يرد خسران من ذيچ ال�
 
 
  message.channel.send(replies[result]).then(message => {
- 			message.react('<a:emoji_91:842129244343369808>').then(r => {
- 				message.react('<a:emoji_94:842141553262460968>');
+ 			message.react('<a:alven:871381046007050253>').then(r => {
+ 				message.react('<a:alven:871381176928059412>');
             });
            });
        }
