4b59c987ef970e807c5b7707e640d92226595dab
AL V EN <78151999+ALVEN-bit@users.noreply.github.com>
2021-08-02 12:52:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -694,23 +694,7 @@ client.on('message', msg => {
       msg.channel.send('```DONE```')
     }
 });
-////////////////////////
-client.on("message", Zeuz => {
-  if (!Zeuz.guild) return;
-  if (Zeuz.content.startsWith(prefix + "wara")) {
-    if (Zeuz.member.voiceChannel) {
-      Zeuz.member.voiceChannel
-        .join()
-        .then(connection => {
-          Zeuz.reply(" جـۆیـنـم کـرد ");
-        })
-        .catch(console.log);
-    } else {
-      Zeuz.reply(" لـەڤـۆیـس نـیـت ");
-    }
-  }
-}); 
-//////////Ittz ALVEN////////
+
   
   client.on("message", message => {
   if (message.content === PREFIX + "unlock") {
