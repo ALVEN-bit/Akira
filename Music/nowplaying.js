6f7b69c5b352882ba19aff6aeeaa40d2bb85c124
AL V EN <78151999+ALVEN-bit@users.noreply.github.com>
2021-08-08 00:46:22 +0300

    Update nowplaying.js

--- Music/nowplaying.js
+++ Music/nowplaying.js
@@ -1,6 +1,6 @@
 const createBar = require("string-progressbar");
 const { Client, Collection, MessageEmbed } = require("discord.js");
-const { attentionembed } = require("../util/attentionembed"); 
+const { attentionembed } = require("../util/attentionembed");
 const { PREFIX } = require(`../config.json`);
 ////////////////////////////
 //////COMMAND BEGIN/////////
@@ -8,25 +8,25 @@ const { PREFIX } = require(`../config.json`);
 module.exports = {
   name: "nowplaying",
   aliases: ['np',"now-playing","current","current-song"],
-  description: "Show current song",
+  description: "(np)Show current song",
   cooldown: 5,
   edesc: `Type nowplaying in chat, to see which song is currently playing! As well as how long it will take until its finished\nUsage: ${PREFIX}nowplaying`,
-  
+
 execute(message) {
     //if not in a guild return
     if(!message.guild) return;
     //react with approve emoji
-    message.react("✅")
+    message.react("<a:emoji_83:836364481998618644>")
     //get Server Queue
     const queue = message.client.queue.get(message.guild.id);
     //if nothing playing error
     if (!queue) return attentionembed(message, "There is nothing playing.").catch(console.error);
-    //Define the current song 
+    //Define the current song
     const song = queue.songs[0];
     //get current song duration in s
-    let minutes = song.duration.split(":")[0];   
-    let seconds = song.duration.split(":")[1];    
-    let ms = (Number(minutes)*60+Number(seconds));   
+    let minutes = song.duration.split(":")[0];
+    let seconds = song.duration.split(":")[1];
+    let ms = (Number(minutes)*60+Number(seconds));
     //get thumbnail
     let thumb;
     if (song.thumbnail === undefined) thumb = "https://cdn.discordapp.com/attachments/748095614017077318/769672148524335114/unknown.png";
@@ -37,10 +37,10 @@ execute(message) {
     const left = ms - seek;
     //define embed
     let nowPlaying = new MessageEmbed()
-      .setTitle("Now playing")
+      .setTitle("<a:emoji_83:836364481998618644> Now playing")
       .setDescription(`[**${song.title}**](${song.url})`)
       .setThumbnail(song.thumbnail.url)
-      .setColor("#c219d8")
+      .setColor("#146DF6")
       .setFooter("Time Remaining: " + new Date(left * 1000).toISOString().substr(11, 8));
       //if its a stream
       if(ms >= 10000) {
@@ -48,9 +48,9 @@ execute(message) {
         //send approve msg
         return message.channel.send(nowPlaying);
       }
-      //If its not a stream 
+      //If its not a stream
       if (ms > 0 && ms<10000) {
-        nowPlaying.addField("\u200b", "**[" + createBar((ms == 0 ? seek : ms), seek, 25, "▬", "⚪️")[0] + "]**\n**" + new Date(seek * 1000).toISOString().substr(11, 8) + " / " + (ms == 0 ? " ◉ LIVE" : new Date(ms * 1000).toISOString().substr(11, 8))+ "**" , false );
+        nowPlaying.addField("\u200b", "**[" + createBar((ms == 0 ? seek : ms), seek, 25, "▬", "🔘")[0] + "]**\n**" + new Date(seek * 1000).toISOString().substr(11, 8) + " / " + (ms == 0 ? " ◉ LIVE" : new Date(ms * 1000).toISOString().substr(11, 8))+ "**" , false );
         //send approve msg
         return message.channel.send(nowPlaying);
       }
