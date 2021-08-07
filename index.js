18df3a32b02738940299eba88a88db2a182a1b93
AL V EN <78151999+ALVEN-bit@users.noreply.github.com>
2021-08-07 18:44:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -936,6 +936,30 @@ client.on("message", message => {
 });
 
 
+client.on('message', message => {
+    if (message.content.startsWith(",stats")) {
+    message.channel.send({
+        embed: new Discord.MessageEmbed()
+            .addField('Uptime', timeCon(process.uptime()), true)
+            .addField('RAM Usage', ${(process.memoryUsage().rss / 1048576).toFixed()}MB, true)
+            .addField('Guild Count', client.guilds.cache.size, true)
+    })
+}
+});
+ 
+function timeCon(time) {
+    let days = Math.floor(time % 31536000 / 86400)
+    let hours = Math.floor(time % 31536000 % 86400 / 3600)
+    let minutes = Math.floor(time % 31536000 % 86400 % 3600 / 60)
+    let seconds = Math.round(time % 31536000 % 86400 % 3600 % 60)
+    days = days > 9 ? days : '0' + days
+    hours = hours > 9 ? hours : '0' + hours
+    minutes = minutes > 9 ? minutes : '0' + minutes
+    seconds = seconds > 9 ? seconds : '0' + seconds
+    return ${days > 0 ? ${days}: : ''}${(hours || days) > 0 ? ${hours}: : ''}${minutes}:${seconds}
+}
+
+
 client.on('ready', () => {
 var channel = client.channels.cache.get('840964396665929760');
     if (channel) channel.join();
