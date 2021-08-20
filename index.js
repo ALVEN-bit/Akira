7c1d4e60869c3627ea38009544f047b33db737ed
AL V EN <78151999+ALVEN-bit@users.noreply.github.com>
2021-08-20 15:05:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,43 @@ const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, `\\$&`);
 //this fires when the BOT STARTS DO NOT TOUCH
 
 client.on("message", message => {
+  if (message.content.startsWith(PREFIX + "panime")) {
+    let man = [
+  "https://cdn.discordapp.com/attachments/737803691565907991/814870994530861116/360cf7ac7e7b9f1441d0948e6ab83f07.jpg",
+  "https://cdn.discordapp.com/attachments/737803691565907991/814870981378048060/b6c9cc93d77714096754e988064d3dee.jpg",
+  "https://cdn.discordapp.com/attachments/737803691565907991/813879559409369109/MOLLY_BUNNY.jpg",
+  "https://cdn.discordapp.com/attachments/737803691565907991/805865899697242162/image0.jpg",
+  "https://cdn.discordapp.com/attachments/737803691565907991/799748841246949426/e760eefc-af79-4003-b9d8-9b42b1a2fdb5.jpg",
+  "https://cdn.discordapp.com/attachments/737803691565907991/799748841246949426/e760eefc-af79-4003-b9d8-9b42b1a2fdb5.jpg",
+  "https://cdn.discordapp.com/attachments/737803691565907991/814870858308780072/1d0b737fcc90657957a42034077f299a.jpg",
+  "https://cdn.discordapp.com/attachments/737803691565907991/810183657021112320/image1.jpg",
+  "https://cdn.discordapp.com/attachments/737803691565907991/804092919928979476/Ash_imboredsoherewearez.png",
+  "https://cdn.discordapp.com/attachments/737803691565907991/813879559409369109/MOLLY_BUNNY.jpg",
+  "https://cdn.discordapp.com/attachments/737803691565907991/805865568036847676/image0.jpg",
+  "https://cdn.discordapp.com/attachments/737803691565907991/804092913956945991/S_H_O_P.jpg",
+  "https://cdn.discordapp.com/attachments/737803691565907991/814871045508956180/99cf9ff40f47e1f3faf0f85f78180f4c.jpg",
+  "https://cdn.discordapp.com/attachments/737803691565907991/804092921480609803/MelDream_Meldream_0190.jpg",
+  "https://cdn.discordapp.com/attachments/737803691565907991/814870880378945566/b4a97fcecac0bbdce829914ebea72322.jpg"
+ ];
+
+    message.channel
+      .send({
+        embed: {
+          description: `${message.author.username} ** Picture Anime **`,
+          image: {
+            url: man[Math.floor(Math.random() * man.length)]
+          }
+        }
+      })
+      .catch(e => {
+        client.log.error(e);
+      });
+  }
+});
+
+
+
+client.on("message", message => {
   if (message.content.startsWith(PREFIX + "pgirl")) {
     let man = [
   "https://cdn.discordapp.com/attachments/737803070217650227/805674220192530462/1b2aa8e3e516bdd7.jpg",
