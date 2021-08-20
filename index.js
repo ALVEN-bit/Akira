79bdf06e0595bed4ae4af9dc262361f515dd70d3
AL V EN <78151999+ALVEN-bit@users.noreply.github.com>
2021-08-20 15:25:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -372,7 +372,6 @@ client.on("message", message => {
         "https://cdn.discordapp.com/attachments/755893014915711047/828893906656690186/KAPTANman_48.gif",
         "https://cdn.discordapp.com/attachments/755893014915711047/829594717859348480/20.gif",
         "https://cdn.discordapp.com/attachments/755893014915711047/827979186257526814/ENES_ACAR_GIF_114_-_Kopya.gif",
-        "https://cdn.discordapp.com/attachments/755893014915711047/827978957668220928/Man_319.
         "https://cdn.discordapp.com/attachments/755893014915711047/827978869209563196/Man_137.gif",
         "https://cdn.discordapp.com/attachments/755893014915711047/827978647142006804/Man_214.gif",
         "https://cdn.discordapp.com/attachments/755893014915711047/827683207914323988/a_d4e861205473258d19542f451ed1083e.gif",
