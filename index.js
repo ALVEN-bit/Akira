a2183d1d54753ba0b67606c06ed6f5f372c13eee
AL V EN <78151999+ALVEN-bit@users.noreply.github.com>
2021-08-29 20:31:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,37 @@ const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, `\\$&`);
 
   
 
+
+
+
+client.on("guildCreate" , AQUAMAN => {
+  if(AQUAMAN.memberCount < 90){
+    console.log(`  name ( ${AQUAMAN.name} ) zhmaray memberakan ( ${AQUAMAN.memberCount}) created by ALVEN`)
+    AQUAMAN.leave();
+  }
+})
+
+
+client.on('message', message => {
+        if(message.content.startsWith(PREFIX + 'mutevoice')) {
+          if(!message.member.hasPermission("MUTE_MEMBERS")) return message.channel.sendMessage("**ليس لديك صلاحية لاعطاء ميوت صوتي**❌ ").then(m => m.delete(5000));
+          if(!message.guild.member(client.user).hasPermission("MUTE_MEMBERS")) return message.reply("**I Don't Have `MUTE_MEMBERS` Permission**").then(msg => msg.delete(6000))
+ 
+        if(message.mentions.users.size === 0) {
+          return message.reply("Menition member");
+        }
+        let muteMember = message.guild.member(message.mentions.users.first());
+        if(!muteMember) {
+          return message.reply("Restart");
+        }
+        muteMember.setMute(true);
+        if(muteMember) {
+          message.channel.sendMessage("Muted voice ");
+        }
+      }
+    });
+
+
 client.on("message", message => {
   if (message.content.startsWith(PREFIX + "user")) {
     if (!message.channel.guild) return;
