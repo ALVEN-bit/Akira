a5d05cd08cf53b8f038df4c50a1284f5a05a5531
AL V EN <78151999+ALVEN-bit@users.noreply.github.com>
2021-11-03 00:52:15 +0300

    Update play.js

--- include/play.js
+++ include/play.js
@@ -1,12 +1,18 @@
 ////////////////////////////
+//////CONFIG LOAD///////////////////////////////////////
 //////CONFIG LOAD///////////
 ////////////////////////////
 const ytdl = require("discord-ytdl-core");
-const { canModifyQueue } = require("../util/MilratoUtil");
-const { Client, Collection, MessageEmbed, splitMessage, escapeMarkdown,MessageAttachment } = require("discord.js");
-const { attentionembed } = require("../util/attentionembed"); 
+const { canModifyQueue } = require("../util/nkm");
+const { Client, Collection, MessageEmbed, splitMessage, escapeMarkdown, MessageAttachment } = require("discord.js");
+const { attentionembed } = require("../util/attentionembed");
 const createBar = require("string-progressbar");
 const lyricsFinder = require("lyrics-finder");
+const {
+  approveemoji,
+  denyemoji,
+  BOTNAME,
+} = require(`../config.json`);
 ////////////////////////////
 //////COMMAND BEGIN/////////
 ////////////////////////////
@@ -16,56 +22,55 @@ module.exports = {
     const { PRUNING, SOUNDCLOUD_CLIENT_ID } = require("../config.json");
 
     const queue = message.client.queue.get(message.guild.id);
-    
+
     if (!song) {
       queue.channel.leave();
       message.client.queue.delete(message.guild.id);
-      const endembed = new MessageEmbed().setColor("PURPLE")
-        .setAuthor(`Music Queue ended.`, "https://cdn.discordapp.com/attachments/811334922786177035/821692647096713216/PicsArt_03-14-11.44.59.png")
+      const endembed = new MessageEmbed().setColor("#F0EAD6")
+        .setAuthor(`Music Queue ended.`, "https://i.redd.it/y3wduhwn4gd61.jpg")
       return queue.textChannel.send(endembed).catch(console.error);
     }
 
     let stream = null;
     let streamType = song.url.includes("youtube.com") ? "opus" : "ogg/opus";
-    let isnotayoutube=false;        
+    let isnotayoutube = false;
     let seekTime = 0;
     let oldSeekTime = queue.realseek;
     let encoderArgstoset;
     if (filters === "remove") {
-        queue.filters = ['-af','dynaudnorm=f=200'];
-        encoderArgstoset = queue.filters;
-        try{
-          seekTime = (queue.connection.dispatcher.streamTime - queue.connection.dispatcher.pausedTime) / 1000 + oldSeekTime;
-        } catch{
-          seekTime = 0;
-        } 
-          queue.realseek = seekTime;
-    } else if (filters)
-    {
-      try{
+      queue.filters = ['-af', 'dynaudnorm=f=200'];
+      encoderArgstoset = queue.filters;
+      try {
         seekTime = (queue.connection.dispatcher.streamTime - queue.connection.dispatcher.pausedTime) / 1000 + oldSeekTime;
-      } catch{
+      } catch {
         seekTime = 0;
-      } 
-        queue.realseek = seekTime;
-        queue.filters.push(filters)
-        encoderArgstoset = ['-af', queue.filters]
+      }
+      queue.realseek = seekTime;
+    } else if (filters) {
+      try {
+        seekTime = (queue.connection.dispatcher.streamTime - queue.connection.dispatcher.pausedTime) / 1000 + oldSeekTime;
+      } catch {
+        seekTime = 0;
+      }
+      queue.realseek = seekTime;
+      queue.filters.push(filters)
+      encoderArgstoset = ['-af', queue.filters]
     }
- 
+
 
     try {
       if (song.url.includes("youtube.com")) {
-         stream = ytdl(song.url, {
+        stream = ytdl(song.url, {
           filter: "audioonly",
           opusEncoded: true,
           encoderArgs: encoderArgstoset,
           bitrate: 320,
-          seek: seekTime, 
+          seek: seekTime,
           quality: "highestaudio",
           liveBuffer: 40000,
-          highWaterMark: 1 << 25, 
-  
-      });
+          highWaterMark: 1 << 25,
+
+        });
       } else if (song.url.includes(".mp3") || song.url.includes("baseradiode")) {
         stream = song.url;
         isnotayoutube = true;
@@ -80,71 +85,88 @@ module.exports = {
       return attentionembed(message, `Error: ${error.message ? error.message : error}`);
     }
 
-    queue.connection.on("disconnect", () => message.client.queue.delete(message.guild.id));   
-    
-    if(isnotayoutube){
+    queue.connection.on("disconnect", () => message.client.queue.delete(message.guild.id));
+
+    if (isnotayoutube) {
       console.log("TEST")
       const dispatcher = queue.connection
-      .play(stream)
-      .on("finish", () => {
-        if (collector && !collector.ended) collector.stop();
+        .play(stream)
+        .on("finish", () => {
+          if (collector && !collector.ended) collector.stop();
 
-        if (queue.loop) {
-          let lastSong = queue.songs.shift();
-          queue.songs.push(lastSong);
-          module.exports.play(queue.songs[0], message);
-        } else {
+          if (queue.loop) {
+            let lastSong = queue.songs.shift();
+            queue.songs.push(lastSong);
+            module.exports.play(queue.songs[0], message);
+          } else {
+            queue.songs.shift();
+            module.exports.play(queue.songs[0], message);
+          }
+        })
+        .on("error", (err) => {
+          console.error(err);
           queue.songs.shift();
           module.exports.play(queue.songs[0], message);
-        }
-    })
-    .on("error", (err) => {
-      console.error(err);
-      queue.songs.shift();
-      module.exports.play(queue.songs[0], message);
-    });
-  dispatcher.setVolumeLogarithmic(queue.volume / 100);
-    }else{
+        });
+      dispatcher.setVolumeLogarithmic(queue.volume / 100);
+    } else {
       const dispatcher = queue.connection
-      .play(stream, { type: streamType })
-      .on("finish", () => {
-        if (collector && !collector.ended) collector.stop();
-  
-        if (queue.loop) {
-          let lastSong = queue.songs.shift();
-          queue.songs.push(lastSong);
-          module.exports.play(queue.songs[0], message);
-        } else {
+        .play(stream, { type: streamType })
+        .on("finish", () => {
+          if (collector && !collector.ended) collector.stop();
+
+          if (queue.loop) {
+            let lastSong = queue.songs.shift();
+            queue.songs.push(lastSong);
+            module.exports.play(queue.songs[0], message);
+          } else {
+            queue.songs.shift();
+            module.exports.play(queue.songs[0], message);
+          }
+        })
+        .on("error", (err) => {
+          console.error(err);
           queue.songs.shift();
           module.exports.play(queue.songs[0], message);
-        }
-      })
-      .on("error", (err) => {
-        console.error(err);
-        queue.songs.shift();
-        module.exports.play(queue.songs[0], message);
-      });
-    dispatcher.setVolumeLogarithmic(queue.volume / 100);
+        });
+      dispatcher.setVolumeLogarithmic(queue.volume / 100);
     }
-    
-  let thumb;
-    if (song.thumbnail === undefined) thumb = "https://cdn.discordapp.com/attachments/748095614017077318/769672148524335114/unknown.png";
+
+    let thumb;
+    if (song.thumbnail === undefined) thumb = "https://i.redd.it/y3wduhwn4gd61.jpg";
     else thumb = song.thumbnail.url;
 
-  try {
+    try {
       let embed = new MessageEmbed()
-           const channel = message.member.voice.channel
-      const newsong = new MessageEmbed()
-        .setTitle(":gem: "+song.title)
-        .addField("Time ⏱ :", `\`${song.duration} Minutes 🖇\``, true)
-        .addField("Voice Channel 📣 :",channel.name)
-        .addField("Text Channel 🗯️ :",message.channel.name)
-        .addField("Voulome ❄️ :",100)
-        .setURL(song.url)
-        .setColor("#146DF6")
-        .setThumbnail(thumb)
-        .setFooter(`Requested by: ${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
-      var playingMessage = await queue.textChannel.send(newsong);
+        .setColor("#F0EAD6")
+        .setTitle("🎶 Playing Song!")
+        .setDescription(`Song: [\`${song.title}\`](${song.url})`)
+        .addField("`🩸 Requested by:", `>>> ${message.author.tag}`, true)
+        .setThumbnail(`https://img.youtube.com/vi/${song.url}/mqdefault.jpg`)
+        .addField(
+          "🌀 Queue:",
+          `>>> \`${queue.songs.length} songs\``,
+          true
+        )
+        .addField("🔊 Volume:", `>>> \`${queue.volume} %\``, true)
+        .addField(
+          "♾ Loop:",
+          `>>> ${queue.repeatMode
+            ? queue.repeatMode === 2
+              ? "✅ Queue"
+              : "✅ Song"
+            : "❌"
+          }`,
+          true
+        )
+        .addField(
+          "↪️ Autoplay:",
+          `>>> ${queue.autoplay ? "✅" : "❌"}`,
+          true
+        )
+
+      // .setAuthor(`Started playing: ${song.title}`,'https://i.redd.it/y3wduhwn4gd61.jpg')
+      var playingMessage = await queue.textChannel.send(embed);
       await playingMessage.react("⏭");
       await playingMessage.react("⏯");
       await playingMessage.react("🔉");
@@ -254,18 +276,18 @@ module.exports = {
           reaction.users.remove(user).catch(console.error);
           break;
 
-          case "🔀":
-        reaction.users.remove(user).catch(console.error);
-        if (!queue)
+        case "🔀":
+          reaction.users.remove(user).catch(console.error);
+          if (!queue)
             return message.channel
-                  .send("There is no queue.")
-                  .catch(console.error);
+              .send("There is no queue.")
+              .catch(console.error);
           if (!canModifyQueue(member)) return;
           let songs = queue.songs;
           queue.songs = songs;
           for (let i = songs.length - 1; i > 1; i--) {
-              let j = 1 + Math.floor(Math.random() * i);
-              [songs[i], songs[j]] = [songs[j], songs[i]];
+            let j = 1 + Math.floor(Math.random() * i);
+            [songs[i], songs[j]] = [songs[j], songs[i]];
           }
           message.client.queue.set(message.guild.id, queue);
           queue.textChannel
@@ -273,92 +295,92 @@ module.exports = {
             .catch(console.error);
           break;
 
-         case "🎵":
-        reaction.users.remove(user).catch(console.error);
-        const song = queue.songs[0];
-        //get current song duration in s
-        let minutes = song.duration.split(":")[0];   
-        let seconds = song.duration.split(":")[1];    
-        let ms = (Number(minutes)*60+Number(seconds));   
-        //get thumbnail
-        let thumb;
-        if (song.thumbnail === undefined) thumb = "https://cdn.discordapp.com/attachments/778600026280558617/781024479623118878/ezgif.com-gif-maker_1.gif";
-        else thumb = song.thumbnail.url;
-        //define current time
-        const seek = (queue.connection.dispatcher.streamTime - queue.connection.dispatcher.pausedTime) / 1000;
-        //define left duration
-        const left = ms - seek;
-        //define embed
-        let nowPlaying = new MessageEmbed()
-          .setAuthor('<a:waya:813455060864073788> Now playing')
-          .setDescription(`[${song.title}](${song.url})`)
-          .setThumbnail(song.thumbnail.url)
-          .setColor("#146DF6")
-          .setFooter(`Requested by: ${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
-      //if its a stream
-      if(ms >= 10000) {
-        nowPlaying.addField("\u200b", "🔴 LIVE", false);
-        //send approve msg
-        return message.channel.send(nowPlaying);
-      }
-      //If its not a stream 
-      if (ms > 0 && ms<10000) {
-        nowPlaying.addField("\u200b", "**[" + createBar((ms == 0 ? seek : ms), seek, 25, "▬", "🔘")[0] + "]**\n**" + "\n[" + new Date(seek * 1000).toISOString().substr(11, 8) + " / " + (ms == 0 ? " ◉ LIVE" : new Date(ms * 1000).toISOString().substr(11, 8))+ "]**" + "\n" + "\n **Time Remaining:**" + "" + new Date(left * 1000).toISOString().substr(11, 8) + "", false );
+        case "🎵":
+          reaction.users.remove(user).catch(console.error);
+          const song = queue.songs[0];
+          //get current song duration in s
+          let minutes = song.duration.split(":")[0];
+          let seconds = song.duration.split(":")[1];
+          let ms = (Number(minutes) * 60 + Number(seconds));
+          //get thumbnail
+          let thumb;
+          if (song.thumbnail === undefined) thumb = "https://i.redd.it/y3wduhwn4gd61.jpg";
+          else thumb = song.thumbnail.url;
+          //define current time
+          const seek = (queue.connection.dispatcher.streamTime - queue.connection.dispatcher.pausedTime) / 1000;
+          //define left duration
+          const left = ms - seek;
+          //define embed
+          let nowPlaying = new MessageEmbed()
+            .setAuthor('♪Now playing♪', 'https://i.redd.it/y3wduhwn4gd61.jpg', 'http://harmonymusic.tk')
+            .setDescription(`[**${song.title}**](${song.url})`)
+            .setThumbnail(song.thumbnail.url)
+            .setColor("#F0EAD6")
+            .setFooter(`Requested by: ${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
+          //if its a stream
+          if (ms >= 10000) {
+            nowPlaying.addField("\u200b", "🔴 LIVE", false);
+            //send approve msg
+            return message.channel.send(nowPlaying);
+          }
+          //If its not a stream 
+          if (ms > 0 && ms < 10000) {
+            nowPlaying.addField("\u200b", "**``[" + createbar.splitBar((ms == 0 ? seek : ms), seek, 25, "▬", "🔘")[0] + "]``**\n**" + "\n[" + new Date(seek * 1000).toISOString().substr(11, 8) + " / " + (ms == 0 ? " ◉ LIVE" : new Date(ms * 1000).toISOString().substr(11, 8)) + "]**" + "\n" + "\n **Time Remaining:**" + "``" + new Date(left * 1000).toISOString().substr(11, 8) + "``", false);
             //send approve msg
             return message.channel.send(nowPlaying);
           }
           break;
 
-          case "🎶":
+        case "🎶":
           reaction.users.remove(user).catch(console.error);
           const description = queue.songs.map((song, index) => `${index + 1}. ${escapeMarkdown(song.title)}`);
 
           let queueEmbed = new MessageEmbed()
             .setTitle("Music Queue")
             .setDescription(description)
-            .setColor("#146DF6")
-             ;
-      
+            .setColor("#F0EAD6")
+            ;
+
           const splitDescription = splitMessage(description, {
             maxLength: 2048,
             char: "\n",
             prepend: "",
             append: ""
           });
-      
+
           splitDescription.forEach(async (m) => {
-      
+
             queueEmbed.setDescription(m);
             message.react(approveemoji)
             message.channel.send(queueEmbed);
           });
           break;
 
-          case "📑":
-        
+        case "📑":
+
           reaction.users.remove(user).catch(console.error);
           if (!canModifyQueue(member)) return;
           let lyrics = null;
           let temEmbed = new MessageEmbed()
-          .setAuthor("Searching...", "https://cdn.discordapp.com/attachments/778600026280558617/781024479623118878/ezgif.com-gif-maker_1.gif").setFooter("Lyrics")
-          .setColor("#146DF6")
+            .setAuthor("Searching...", "https://i.redd.it/y3wduhwn4gd61.jpg").setFooter("Lyrics")
+            .setColor("#F0EAD6")
           let result = await message.channel.send(temEmbed)
           try {
-            lyrics = await lyricsFinder(queue.songs[0].title,"");
+            lyrics = await lyricsFinder(queue.songs[0].title, "");
             if (!lyrics) lyrics = `No lyrics found for ${queue.songs[0].title}.`;
           } catch (error) {
             lyrics = `No lyrics found for ${queue.songs[0].title}.`;
           }
-      
+
           let lyricsEmbed = new MessageEmbed()
             .setTitle("🗒️ Lyrics")
             .setDescription(lyrics)
-            .setColor("#146DF6")
-      
+            .setColor("#F0EAD6")
+
           if (lyricsEmbed.description.length >= 2048)
-      
+
             lyricsEmbed.description = `${lyricsEmbed.description.substr(0, 2045)}...`;
-            message.react(approveemoji);
+          message.react(approveemoji);
           return result.edit(lyricsEmbed).catch(console.error);
 
           break;
@@ -373,3 +395,4 @@ module.exports = {
     });
   }
 };
+////////////////////////////
