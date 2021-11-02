136553630ee4f7dc4cda625f4a1d435e0d3032d1
AL V EN <78151999+ALVEN-bit@users.noreply.github.com>
2021-11-03 00:48:15 +0300

    Update nowplaying.js

--- Music/nowplaying.js
+++ Music/nowplaying.js
@@ -3,36 +3,40 @@
 ////////////////////////////
 const createBar = require("string-progressbar");
 const { Client, Collection, MessageEmbed } = require("discord.js");
-const { attentionembed } = require("../util/attentionembed");
-const { PREFIX } = require(`../config.json`);
+const { attentionembed } = require("../util/attentionembed"); 
+const {
+  approveemoji,
+  denyemoji,
+  PREFIX,
+} = require(`../config.json`);
 ////////////////////////////
 //////COMMAND BEGIN/////////
 ////////////////////////////
 module.exports = {
   name: "nowplaying",
   aliases: ['np',"now-playing","current","current-song"],
-  description: "\`(np)Show current song\`",
+  description: "Show current song",
   cooldown: 5,
   edesc: `Type nowplaying in chat, to see which song is currently playing! As well as how long it will take until its finished\nUsage: ${PREFIX}nowplaying`,
-
+  
 execute(message) {
     //if not in a guild return
     if(!message.guild) return;
     //react with approve emoji
-    message.react("")
+    message.react(approveemoji)
     //get Server Queue
     const queue = message.client.queue.get(message.guild.id);
     //if nothing playing error
     if (!queue) return attentionembed(message, "There is nothing playing.").catch(console.error);
-    //Define the current song
+    //Define the current song 
     const song = queue.songs[0];
     //get current song duration in s
-    let minutes = song.duration.split(":")[0];
-    let seconds = song.duration.split(":")[1];
-    let ms = (Number(minutes)*60+Number(seconds));
+    let minutes = song.duration.split(":")[0];   
+    let seconds = song.duration.split(":")[1];    
+    let ms = (Number(minutes)*60+Number(seconds));   
     //get thumbnail
     let thumb;
-    if (song.thumbnail === undefined) thumb = "https://cdn.discordapp.com/attachments/748095614017077318/769672148524335114/unknown.png";
+    if (song.thumbnail === undefined) thumb = "https://i.redd.it/y3wduhwn4gd61.jpg";
     else thumb = song.thumbnail.url;
     //define current time
     const seek = (queue.connection.dispatcher.streamTime - queue.connection.dispatcher.pausedTime) / 1000;
@@ -40,20 +44,20 @@ execute(message) {
     const left = ms - seek;
     //define embed
     let nowPlaying = new MessageEmbed()
-      .setTitle("<a:like:813847731285393439> Now playing")
-      .setDescription(`**[${song.title}](${song.url})**`)
-      .setThumbnail(song.thumbnail.url)
-      .setColor("#FC00FF")
-      .setFooter("Time Remaining: " + new Date(left * 1000).toISOString().substr(11, 8));
+          .setAuthor('♪Now playing♪','https://i.redd.it/y3wduhwn4gd61.jpg')
+          .setDescription(`[**${song.title}**](${song.url})`)
+          .setThumbnail(song.thumbnail.url)
+          .setColor("#F0EAD6")
+          .setFooter(`Requested by: ${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
       //if its a stream
       if(ms >= 10000) {
         nowPlaying.addField("\u200b", "🔴 LIVE", false);
         //send approve msg
         return message.channel.send(nowPlaying);
       }
-      //If its not a stream
+      //If its not a stream 
       if (ms > 0 && ms<10000) {
-        nowPlaying.addField("\u200b", "**[" + createBar((ms == 0 ? seek : ms), seek, 25, "▬", "<:currentposition:770098066552258611>")[0] + "]**\n**" + new Date(seek * 1000).toISOString().substr(11, 8) + " / " + (ms == 0 ? " ◉ LIVE" : new Date(ms * 1000).toISOString().substr(11, 8))+ "**" , false );
+        nowPlaying.addField("\u200b", "**``[" + createBar.filledBar((ms == 0 ? seek : ms), seek, 25, "▬", "🔘")[0] + "]``**\n**" + "\n[" + new Date(seek * 1000).toISOString().substr(11, 8) + " / " + (ms == 0 ? " ◉ LIVE" : new Date(ms * 1000).toISOString().substr(11, 8))+ "]**" + "\n" + "\n **Time Remaining:**" + "``" + new Date(left * 1000).toISOString().substr(11, 8) + "``", false );
         //send approve msg
         return message.channel.send(nowPlaying);
       }
