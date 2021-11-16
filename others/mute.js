65e9491c51a1a7db97952f4d74264e3696f5dde7
AL V EN <78151999+ALVEN-bit@users.noreply.github.com>
2021-11-16 22:22:03 +0300

    Update mute.js

--- others/mute.js
+++ others/mute.js
@@ -1,106 +1,53 @@
-const fs = require("fs");
-const Discord = require("discord.js");
+const db = require('quick.db')
 
 module.exports = {
-    config: {
-        name: "mute",
-        description: "Mutes a member in the discord!",
-        usage: "[name | nickname | mention | ID] <reason> (optional)",
-    },
-    run: async (bot, message, args) => {
-        try {
-            if (!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send("**You Dont Have Permmissions To Mute Someone! - [MANAGE_GUILD]**");
+    name: 'mute',
+    description: "Mute a user.",
 
-            if (!message.guild.me.hasPermission("MANAGE_GUILD")) return message.channel.send("**I Don't Have Permissions To Mute Someone! - [MANAGE_GUILD]**")
-            if (!args[0]) return message.channel.send("**Please Enter A User To Be Muted!**");
+    async execute(message, args) {
+        if (!message.member.hasPermission("MANAGE_ROLES")) {
+            return message.channel.send(
+              "You do not have permission to use this command."
+            );
+          }
+      
+          if (!message.guild.me.hasPermission("MANAGE_ROLES")) {
+            return message.channel.send("The bot does not have permission to mute.");
+          }
 
-            var mutee = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args[0].toLocaleLowerCase()) || message.guild.members.cache.find(ro => ro.displayName.toLowerCase() === args[0].toLocaleLowerCase());
-            if (!mutee) return message.channel.send("**Please Enter A Valid User To Be Muted!**");
+          const user = message.mentions.members.first();
 
-            if (mutee === message.member) return message.channel.send("**You Cannot Mute Yourself!**")
-            if (mutee.roles.highest.comparePositionTo(message.guild.me.roles.highest) >= 0) return message.channel.send('**Cannot Mute This User!**')
+    if (!user) {
+      return message.channel.send(
+        "You are required to mention a user you wish to mute."
+      );
+    }
+ 
+    if(user.id === message.author.id) {
+      return message.channel.send("You cannot mute yourself.");
+    }
 
-            let reason = args.slice(1).join(" ");
-            if (mutee.user.bot) return message.channel.send("**Cannot Mute Bots!**");
-            const userRoles = mutee.roles.cache
-                .filter(r => r.id !== message.guild.id)
-                .map(r => r.id)
+    let reason = args.slice(1).join(" ")
+    
+    
+    if(!reason) reason = 'Unspecified';
 
-            let muterole;
-            let dbmute = await db.fetch(`muterole_${message.guild.id}`);
-            let muteerole = message.guild.roles.cache.find(r => r.name === "muted")
+    let muterole = message.guild.roles.cache.find(x => x.name === "Muted")
+    
+    
+      if(!muterole) {
+      return message.channel.send("The guild does not have a mute role. Please create one within Server Settings.")
+    }
 
-            if (!message.guild.roles.cache.has(dbmute)) {
-                muterole = muteerole
-            } else {
-                muterole = message.guild.roles.cache.get(dbmute)
-            }
+    if(user.roles.cache.has(muterole)) {
+        return message.channel.send("The specified user is already muted.")
+      }
+    
+      user.roles.add(muterole)
+    
+      await message.channel.send(`✅ **${message.mentions.users.first().username} muted from the text! 🤐**`)
+          
+          user.send(``)
 
-            if (!muterole) {
-                try {
-                    muterole = await message.guild.roles.create({
-                        data: {
-                            name: "muted",
-                            color: "#514f48",
-                            permissions: []
-                        }
-                    })
-                    message.guild.channels.cache.forEach(async (channel) => {
-                        await channel.createOverwrite(muterole, {
-                            SEND_MESSAGES: false,
-                            ADD_REACTIONS: false,
-                            SPEAK: false,
-                            CONNECT: false,
-                        })
-                    })
-                } catch (e) {
-                    console.log(e);
-                }
-            };
-
-            if (mutee.roles.cache.has(muterole.id)) return message.channel.send("**User Is Already Muted!**")
-
-            db.set(`muteeid_${message.guild.id}_${mutee.id}`, userRoles)
-          try {
-            mutee.roles.set([muterole.id]).then(() => {
-                mutee.send(`**Hello, You Have Been Muted In ${message.guild.name} for - ${reason || "No Reason"}`).catch(() => null)
-            })
-            } catch {
-                 mutee.roles.set([muterole.id])                               
-            }
-                if (reason) {
-                const sembed = new MessageEmbed()
-                    .setColor("GREEN")
-                    .setAuthor(message.guild.name, message.guild.iconURL())
-                    .setDescription(`${mutee.user.username} was successfully muted for ${reason}`)
-                message.channel.send(sembed);
-                } else {
-                    const sembed2 = new MessageEmbed()
-                    .setColor("GREEN")
-                    .setDescription(`${mutee.user.username} was successfully muted`)
-                message.channel.send(sembed2);
-                }
-            
-            let channel = db.fetch(`modlog_${message.guild.id}`)
-            if (!channel) return;
-
-            let embed = new MessageEmbed()
-                .setColor('BLACK')
-                .setThumbnail(mutee.user.displayAvatarURL({ dynamic: true }))
-                .setAuthor(`${message.guild.name} Modlogs`, message.guild.iconURL())
-                .addField("**Moderation**", "mute")
-                .addField("**Mutee**", mutee.user.username)
-                .addField("**Moderator**", message.author.username)
-                .addField("**Reason**", `${reason || "**No Reason**"}`)
-                .addField("**Date**", message.createdAt.toLocaleString())
-                .setFooter(message.member.displayName, message.author.displayAvatarURL())
-                .setTimestamp()
-
-            var sChannel = message.guild.channels.cache.get(channel)
-            if (!sChannel) return;
-            massing.send(embed)
-        } catch {
-            return;
-        }
     }
 }
