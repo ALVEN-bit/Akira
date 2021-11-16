4177acdceb396455b3e431143514ae8e241aa91d
AL V EN <78151999+ALVEN-bit@users.noreply.github.com>
2021-11-16 22:20:59 +0300

    Create count.js

---
+++ others/count.js
@@ -0,0 +1,39 @@
+const { MessageEmbed } = require("discord.js");
+const Color = `RANDOM`;
+
+module.exports = {
+  name: "membercount",
+  aliases: ["count", "infocount"],
+  category: "count",
+  async execute(message, args) {
+    const Members = message.guild.memberCount;
+    const bots = message.guild.members.cache.filter(
+      member => member.bot === true
+    ).size;
+    const humans = message.guild.members.cache.filter(
+      member => !member.user.bot
+    ).size;
+    const online = message.guild.members.cache.filter(
+      member => member.presence.status === "online"
+    ).size;
+    const offline = message.guild.members.cache.filter(
+      member => member.presence.status === "offline"
+    ).size;
+    const dnd = message.guild.members.cache.filter(
+      member => member.presence.status === "dnd"
+    ).size;
+    const idle = message.guild.members.cache.filter(
+      member => member.presence.status === "idle"
+    ).size;
+
+    const embed = new MessageEmbed()
+      .setColor(`#FC00FF`)
+      .setTitle(`Members Information`)
+      .addField(`<a:discord:813406342173818910> | All Members`, Members)
+      .setThumbnail(message.guild.iconURL({ dynamic: true }))
+      .setAuthor(`${message.guild.name}`)
+      .setTimestamp();
+
+    message.channel.send(embed);
+  }
+};
