97471299f7c235c19fafee8146257b55bc142716
AL V EN <78151999+ALVEN-bit@users.noreply.github.com>
2021-11-20 22:44:03 +0300

    Update userinfo.js

--- others/userinfo.js
+++ others/userinfo.js
@@ -11,16 +11,16 @@ module.exports = {
         let status;
         switch (user.presence.status) {
             case "online":
-                status = "<:online:729181184193462285> online";
+                status = "🟢 online";
                 break;
             case "dnd":
-                status = "<:dnd:729181212530442311> dnd";
+                status = "🔴 dnd";
                 break;
             case "idle":
-                status = "<:idle:729181121933475931> idle";
+                status = "🌙 idle";
                 break;
             case "offline":
-                status = "<:offline:729181162182017051> offline";
+                status = "🔘 offline";
                 break;
         }
 
@@ -31,7 +31,7 @@ module.exports = {
             .setThumbnail(user.user.displayAvatarURL({dynamic : true}))
             .addFields(
                 {
-                    name: "<a:KING:820738504036188181> Name : ",
+                    name: "Name : ",
                     value: user.user.username,
                     inline: true
                 },
@@ -46,26 +46,26 @@ module.exports = {
                 },
                 
                 {
-                    name: "<a:waya:813455060864073788> Activity : ",
+                    name: "🥇 Activity : ",
                     value: user.presence.activities[0] ? user.presence.activities[0].name : `User isn't playing a game!`,
                     inline: true
                 },
                 {
-                    name: '<a:links:813729922643263558> Avatar link : ',
+                    name: '🖇 Avatar link : ',
                     value: `[Click Here](${user.user.displayAvatarURL()})`
                 },
                 {
-                    name: '<a:time:813403485902864435> Creation Date : ',
+                    name: '📆 Creation Date : ',
                     value: user.user.createdAt.toLocaleDateString("en-us"),
                     inline: true
                 },
                 {
-                    name: '<a:discord:813406342173818910> Joined Date : ',
+                    name: '📊 Joined Date : ',
                     value: user.joinedAt.toLocaleDateString("en-us"),
                     inline: true
                 },
                 {
-                    name: '<a:KING:820738504036188181> User Roles : ',
+                    name: ' ⚖️ User Roles : ',
                     value: user.roles.cache.map(role => role.toString()).join(" ,"),
                     inline: true
                 }
