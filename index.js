512b44de5018ceafa88cb9566fe19821ff74d807
AL V EN <78151999+ALVEN-bit@users.noreply.github.com>
2021-12-03 20:32:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login("ODI4MzA4OTkzMTcyMDQ1ODQ1.YGns3Q.BmRhtGv96P0WNjMTt7u0BHGLc74");
+client.login("ODI4MzA4OTkzMTcyMDQ1ODQ1.YGns3Q.IPF81liOeGwcEdta-B7BU27pPKw");
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
