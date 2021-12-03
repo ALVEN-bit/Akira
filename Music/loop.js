891635ba8cb15637b8ccf373cdb21839619ef432
AL V EN <78151999+ALVEN-bit@users.noreply.github.com>
2021-12-03 23:09:31 +0300

    Update loop.js

--- Music/loop.js
+++ Music/loop.js
@@ -3,7 +3,7 @@
 ////////////////////////////
 const { canModifyQueue } = require("../util/MilratoUtil");
 const { MessageEmbed } = require("discord.js");
-const { attentionembed } = require("../util/attentionembed");
+const { attentionembed } = require("../util/attentionembed"); 
 const { PREFIX } = require(`../config.json`);
 ////////////////////////////
 //////COMMAND BEGIN/////////
@@ -11,7 +11,7 @@ const { PREFIX } = require(`../config.json`);
 module.exports = {
   name: "loop",
   aliases: ['l'],
-  description: "(l)Toggle music loop",
+  description: "Toggle music loop",
   cooldown: 3,
   edesc: `Just type the Command in the chat to activate/deactivate loop, you can also react to the loop emoji, to receive the same goal!\nUsage: ${PREFIX}loop`,
 async execute(message) {
@@ -21,16 +21,16 @@ async execute(message) {
     const queue = message.client.queue.get(message.guild.id);
     //If no Queue Error
     if (!queue) return attentionembed(message, "There is nothing playing").catch(console.error);
-    //If not in a VOICE
+    //If not in a VOICE 
     if (!await canModifyQueue(message.member)) return;
     //Reverse the Loop state
     queue.loop = !queue.loop;
     //Define the Loop embed
     const loopembed = new MessageEmbed()
-    .setColor(queue.loop ? "RANDOM" : "RANDOM")
-    .setAuthor(`Loop is now ${queue.loop ? " enabled" : " disabled"}`, "")
+    .setColor(queue.loop ? "#c219d8" : "#ff0e7a")
+    .setAuthor(`Loop is now ${queue.loop ? " enabled" : " disabled"}`, "https://cdn.discordapp.com/emojis/769913064194834511.png")
     //react with approve emoji
-    message.react("<:emoji_4:822203026776391711>");
+    message.react("✅");
     //send message into the Queue chat
     return queue.textChannel
       .send(loopembed)
